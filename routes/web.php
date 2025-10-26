5d0f588bd009f55cd2b480bd2ab0781bde416a72
sekarsekarrrr <prmsths@gmail.com>
2025-10-27 00:20:30 +0700

    route dan controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdukController;
 
+Route::get('/home', function () {
+    return view('welcome');
+});
+Route::get('/index', function () {
+    echo 'welcome';
+});
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/show/{nama}', function ($nama) {
+    return "hallo,".$nama;
+});
+Route::get('/edit/{nama}', function ($nama) {
+    return "hallo,".$nama;
+})->where('nama','[A-Za-z]+');
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+Route::get('/produk', [ProdukController::class,'index']);
+
+
